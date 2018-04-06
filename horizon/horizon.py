02a9e477e13e4b99394764279b605ff84bbf89c8
retnikt <_@retnikt.uk>
2018-04-06 15:07:35 +0100

    refactor config load to different method

--- horizon/horizon.py
+++ horizon/horizon.py
@@ -4,27 +4,30 @@ import json
 
 class Horizon(http.server.HTTPServer):
     def __init__(self, config_dir):
+
+        self.config = {}
+        self.load_config(config_dir)
+
         # default address to bind to
         address = ("0.0.0.0", 80)
+        super(Horizon, self).__init__(address, HorizonHandler)
 
+    def load_config(self, config_dir):
         try:
             with open(f"{config_dir}/horizon.json") as f:
                 self.config = json.load(f)
         except FileNotFoundError:
-            # create config file
-            pass
-        # noinspection PyBroadException
-        try:
-            open(f"{config_dir}/horizon.json", 'x').close()
-            self.config = {}
-        except OSError as e:
-            print(f"OSError {e.errno}: Could not create Horizon configuration file.")
-            exit(e.errno)
-        except Exception as e:
-            # print error info
-            print(f"{type(e).__name__}: {' '.join([str(i) for i in e.args])}")
-
-        super(Horizon, self).__init__(address, HorizonHandler)
+            # noinspection PyBroadException
+            try:
+                # create config file
+                open(f"{config_dir}/horizon.json", 'x').close()
+            except OSError as e:
+                print(f"OSError {e.errno}: Could not create Horizon configuration file.")
+                exit(e.errno)
+            except Exception as e:
+                # print error info
+                print(f"{type(e).__name__}: {' '.join([str(i) for i in e.args])}")
+                exit(1)
 
 
 class HorizonHandler(http.server.BaseHTTPRequestHandler):

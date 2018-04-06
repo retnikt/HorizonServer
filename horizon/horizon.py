b67d34ad0d880b31cfc884b77ce2f8959786e449
retnikt <_@retnikt.uk>
2018-04-06 16:34:35 +0100

    refactor config create to separate method

--- horizon/horizon.py
+++ horizon/horizon.py
@@ -6,28 +6,32 @@ class Horizon(http.server.HTTPServer):
     def __init__(self, config_dir):
 
         self.config = {}
-        self.load_config(config_dir)
+        self.config_dir = config_dir
+        self.load_config()
 
         # default address to bind to
         address = ("0.0.0.0", 80)
         super(Horizon, self).__init__(address, HorizonHandler)
 
-    def load_config(self, config_dir):
+    def load_config(self):
         try:
-            with open(f"{config_dir}/horizon.json") as f:
+            with open(f"{self.config_dir}/horizon.json") as f:
                 self.config = json.load(f)
         except FileNotFoundError:
-            # noinspection PyBroadException
-            try:
-                # create config file
-                open(f"{config_dir}/horizon.json", 'x').close()
-            except OSError as e:
-                print(f"OSError {e.errno}: Could not create Horizon configuration file.")
-                exit(e.errno)
-            except Exception as e:
-                # print error info
-                print(f"{type(e).__name__}: {' '.join([str(i) for i in e.args])}")
-                exit(1)
+            self.create_config()
+
+    # noinspection PyBroadException
+    def create_config(self):
+        try:
+            # create config file
+            open(f"{self.config_dir}/horizon.json", 'x').close()
+        except OSError as e:
+            print(f"OSError {e.errno}: Could not create Horizon configuration file.")
+            exit(e.errno)
+        except Exception as e:
+            # print error info
+            print(f"{type(e).__name__}: {' '.join([str(i) for i in e.args])}")
+            exit(1)
 
 
 class HorizonHandler(http.server.BaseHTTPRequestHandler):

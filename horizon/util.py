44f5d1a2cd18a269092f064ec1c64c6ff7e809f3
retnikt <_@retnikt.uk>
2018-04-07 11:03:23 +0100

    improvements to AttrDict

--- horizon/util.py
+++ horizon/util.py
@@ -3,5 +3,10 @@ class AttrDict:
     A special object that sets its __dict__ to the passed dict.
     Allows getattr instead of getitem for easier code writing.
     """
-    def __init__(self, d):
-        self.__dict__ = d
+    def __init__(self, d: dict):
+        # recursively turn all nested dicts inside to AttrDict s
+        self.__dict__ = {k: AttrDict(v) if isinstance(v, dict) else v for k, v in d.items()}
+
+    def __repr__(self): return repr(self.__dict__)
+
+    def __str__(self): return str(self.__dict__)
